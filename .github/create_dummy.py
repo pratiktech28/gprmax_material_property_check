0b914704a2f06db31d2ce51cf77a9f01c46d88a4
Prateek sharma <pratiktech28@gmail.com>
2026-03-23 20:34:08 +0530

    Update create_dummy.py

--- .github/create_dummy.py
+++ .github/create_dummy.py
@@ -2,7 +2,6 @@ import h5py
 import numpy as np
 with h5py.File('material_test.out', 'w') as f:
     g = f.create_group('src_0')
-    # Ek sin wave daal dete hain jo hamare ref_trace se match kare
     data = np.sin(np.linspace(0, 10, 100))
     g.create_dataset('Ez', data=data)
 print("Dummy material_test.out created!")
